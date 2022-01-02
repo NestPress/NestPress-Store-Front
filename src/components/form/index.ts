fa3ab37aa4364831c472f22058898af508bdc2de
dadmor <dadmor@gmail.com>
2022-01-02 16:45:21 +0100

    add public to gitignore

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -2,6 +2,7 @@ export * from "./InputField";
 export * from "./SelectField";
 export * from "./TextareaField";
 export * from "./KeyValueField";
+export * from "./ButtonsField";
 export * from "./SubmitButton";
 export * from "./Form";
 
