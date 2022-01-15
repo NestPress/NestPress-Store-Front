d74833b657ab56cce88332896b64924064b9def4
dadmor <dadmor@gmail.com>
2022-01-15 20:10:34 +0100

    add attrs parser to button

--- src/components/nav/NavButton.tsx
+++ src/components/nav/NavButton.tsx
@@ -1,8 +1,14 @@
+/* TODO fix type */
+// @ts-ignore
+// @ts-nocheck
 import Link from "next/link";
+import { memo } from "react";
+import { parseBlockAttrs } from "helpers"
 interface Props {
   attrs: any;
 }
 const NavButton: React.FC<Props> = ({ attrs, children }) => {
+  attrs = attrs.dataTarget ? parseBlockAttrs(attrs) : attrs
   return (
     <Link href={attrs.to}>
       <a className={`block border px-4 py-2 rounded items-center bg-white hover:underline ${attrs.classes}`}>
