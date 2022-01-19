123b2982117f3c2690aea1b605b14cb6abea96b5
dadmor <dadmor@gmail.com>
2022-01-19 23:05:00 +0100

    add mapbox

--- src/components/data/MapBox.tsx
+++ src/components/data/MapBox.tsx
@@ -31,3 +31,56 @@ const MapBox: React.FC<Props> = memo(({ attrs, children }) => {
   );
 });
 export default MapBox;
+{/*<ReactMapGL
+        className={tw`h-screen`}
+        {...viewport}
+        mapStyle={new Date().getHours() > 20 ? 'mapbox://styles/mapbox/dark-v10' : 'mapbox://styles/mapbox/light-v10'}
+        mapboxApiAccessToken={MB_TOKEN}
+        onViewportChange={(nextViewport: any) => setViewport(nextViewport)}
+      >
+        {profileData?.getProfile?.location && !profileData?.getProfile?.hideMeOnMap && (
+          <Marker
+            latitude={profileData.getProfile.location.lat}
+            longitude={profileData.getProfile.location.long}
+          >
+            {profileData.getProfile.featuredAsset?.preview ? <GetImage
+              src={profileData.getProfile.featuredAsset?.preview}
+              alt=""
+              className={tw`rounded-full h-6 w-6`}
+              onClick={() => navigate(`/me`)}
+            /> : getProfileIcon(profileData?.getProfile?.profileType || '', profileData?.getProfile?.id)}
+          </Marker>
+        )}
+        {dataFindProfiles?.findProfiles?.list.flatMap((e) =>
+          e && e.location && !e.hideMeOnMap ? (
+            <Marker
+              key={e.id}
+              latitude={e.location.lat}
+              longitude={e.location.long}
+            >
+              {e.featuredAsset?.preview ? <GetImage
+                src={e.featuredAsset?.preview}
+                alt=""
+                className={tw`rounded-full h-6 w-6`}
+                onClick={() => navigate(`/user/${e.id}`)}
+              /> : getProfileIcon(e?.profileType || '', e.id)}
+            </Marker>
+          ) : null
+        )}
+        {data?.getEvents.flatMap((e) =>
+          e ? (
+            <Marker
+              key={e.id}
+              latitude={e.location.lat}
+              longitude={e.location.long}
+            >
+              <GetImage
+                src={IconCalendarMain}
+                alt=""
+                className={tw`rounded-full h-6 w-6 bg-white`}
+                onClick={() => navigate(`/event/${e.id}`)}
+              />
+            </Marker>
+          ) : null
+        )}
+      </ReactMapGL>*/}
